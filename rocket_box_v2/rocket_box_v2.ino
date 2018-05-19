0c64ea034d1144d64782306d6f8c211cd95a799b
Cheeto836 <mprussell836@gmail.com>
2018-05-19 18:45:46 -0400

    updated constant names to be caps

--- rocket_box_v2/rocket_box_v2.ino
+++ rocket_box_v2/rocket_box_v2.ino
@@ -1,24 +1,23 @@
-
 // Deadman switch to control firing authority to prevent accidental / unpermitted firing 
-const int deadmanSwitchPin = 10;
+const int DEADMAN_SWITCH_PIN = 10;
 
 // Gigantic red FIRE button
-const int firePin = 7;
+const int FIRE_PIN = 7;
 
 // Rocker controlled enabled switches
-const int leftEnabledPin = 8;
-const int rightEnabledPin = 9;
+const int LEFT_ENABLE_PIN = 8;
+const int RIGHT_ENABLE_PIN = 9;
 
 // Outputs
-const int fireLightPin = 2;
-const int leftFirePin = 3;
-const int rightFirePin = 4;
+const int FIRE_LIGHT_PIN = 2;
+const int LEFT_FIRE_PIN = 3;
+const int RIGHT_FIRE_PIN = 4;
 
 // Time that solenoids will be open
-const int fireTime = 200;
+const int FIRE_TIME = 200;
 
 // Delay after firing to allow system to recharge
-const int fireDelay = 2000;
+const int FIRE_DELAY = 2000;
 
 // Defaults to HIGH because SainSmart is trigged on logic low
 int fireLightState = HIGH;
@@ -32,29 +31,29 @@ void setup() {
 
   Serial.begin(9600);
 
-  digitalWrite(fireLightPin, HIGH);
-  digitalWrite(leftFirePin, HIGH);
-  digitalWrite(rightFirePin, HIGH);
+  digitalWrite(FIRE_LIGHT_PIN, HIGH);
+  digitalWrite(LEFT_FIRE_PIN, HIGH);
+  digitalWrite(RIGHT_FIRE_PIN, HIGH);
 
-  pinMode(fireLightPin, OUTPUT);
-  pinMode(leftFirePin, OUTPUT);
-  pinMode(rightFirePin, OUTPUT);
+  pinMode(FIRE_LIGHT_PIN, OUTPUT);
+  pinMode(LEFT_FIRE_PIN, OUTPUT);
+  pinMode(RIGHT_FIRE_PIN, OUTPUT);
 
-  pinMode(deadmanSwitchPin, INPUT);
-  pinMode(firePin, INPUT);
-  pinMode(leftEnabledPin, INPUT);
-  pinMode(rightEnabledPin, INPUT); 
+  pinMode(DEADMAN_SWITCH_PIN, INPUT);
+  pinMode(FIRE_PIN, INPUT);
+  pinMode(LEFT_ENABLE_PIN, INPUT);
+  pinMode(RIGHT_ENABLE_PIN, INPUT); 
 
 }
 
 void loop() {
 
-  int leftEnabledState = digitalRead(leftEnabledPin);
-  int rightEnabledState = digitalRead(rightEnabledPin);
+  int leftEnabledState = digitalRead(LEFT_ENABLE_PIN);
+  int rightEnabledState = digitalRead(RIGHT_ENABLE_PIN);
 
-  int deadmanSwitchState = digitalRead(deadmanSwitchPin);
+  int deadmanSwitchState = digitalRead(DEADMAN_SWITCH_PIN);
 
-  int fire = digitalRead(firePin);
+  int fire = digitalRead(FIRE_PIN);
 
   // If either of the rockets are enabled and the deadman switch is pressed
   if( deadmanSwitchState == HIGH && ( ( leftEnabledState == HIGH ) || ( rightEnabledState == HIGH ) ) )
@@ -71,7 +70,7 @@ void loop() {
   {
     // Update the fire enable state
     prevFireLightState = fireLightState;
-    digitalWrite(fireLightPin, fireLightState);
+    digitalWrite(FIRE_LIGHT_PIN, fireLightState);
 
     if( fireLightState == LOW )
     {
@@ -94,32 +93,32 @@ void loop() {
       {
         // Only firing left rocket
         Serial.println("Firing Left Rocket");
-        digitalWrite(leftFirePin, LOW);
-        delay(fireTime);
-        digitalWrite(leftFirePin, HIGH);
-        delay(fireDelay);
+        digitalWrite(LEFT_FIRE_PIN, LOW);
+        delay(FIRE_TIME);
+        digitalWrite(LEFT_FIRE_PIN, HIGH);
+        delay(FIRE_DELAY);
         prevFired = 1;
       }
       else if( leftEnabledState == LOW && rightEnabledState == HIGH )
       {
         // Only firing right rocket
         Serial.println("Firing Right Rocket");
-        digitalWrite(rightFirePin, LOW);
-        delay(fireTime);
-        digitalWrite(rightFirePin, HIGH);
-        delay(fireDelay);
+        digitalWrite(RIGHT_FIRE_PIN, LOW);
+        delay(FIRE_TIME);
+        digitalWrite(RIGHT_FIRE_PIN, HIGH);
+        delay(FIRE_DELAY);
         prevFired = 1;
       }
       else if( leftEnabledState == HIGH && rightEnabledState == HIGH )
       {
         // Fire both rockets!
         Serial.println("Firing Both Rockets");
-        digitalWrite(leftFirePin, LOW);
-        digitalWrite(rightFirePin, LOW);
-        delay(fireTime);
-        digitalWrite(leftFirePin, HIGH);
-        digitalWrite(rightFirePin, HIGH);
-        delay(fireDelay);
+        digitalWrite(LEFT_FIRE_PIN, LOW);
+        digitalWrite(RIGHT_FIRE_PIN, LOW);
+        delay(FIRE_TIME);
+        digitalWrite(LEFT_FIRE_PIN, HIGH);
+        digitalWrite(RIGHT_FIRE_PIN, HIGH);
+        delay(FIRE_DELAY);
         prevFired = 1;
       }
     }
